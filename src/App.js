0b5d03d6b0114a5693118d4b8322e0857fabc27b
Sumit Pawar <sumitpawar@Sumits-MBP-2.attlocal.net>
2021-04-02 21:58:57 -0700

    adding missing files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Sweet N Spicy Channel
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
